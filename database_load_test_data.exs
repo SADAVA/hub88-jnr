cf03f844b2c620e1ccea0da759a10508fc7d46d6
SADAVA <sadava@protonmail.com>
2022-02-26 22:45:01 +0200

    Ecto still rejects to work

--- database_load_test_data.exs
+++ database_load_test_data.exs
@@ -1,21 +1,28 @@
 
 
-Operator.Accounts.create_user(%{
-  name: "asd"
-})
 
 Operator.Accounts.create_user(%{
-  name: "john12345",
-  request_uuid: "583c985f-fee6-4c0e-bbf5-308aad6265af",
-  country: Operator.Repo.get_by!(Operator.Common.Country, name: "FR"),
-  jurisdiction: Operator.Repo.get_by!(Operator.Common.Jurisdiction, name: "MGA"),
-  sub_partner_id: "123_sub",
-  birth_date: "1980-10-20",
-  registration_date: "2010-10-20",
-  tags: [
-    "VIP",
-    "affiliate"
-  ],
-  sex: "MALE",
-  affiliate_id: "124_af"
+	name: "john12345"
 })
+
+Operator.Repo.get_by!(Operator.Accounts.User, name: "john12345")
+
+
+
+
+# Operator.Accounts.create_user(%{
+#   name: "john12345",
+#   request_uuid: "583c985f-fee6-4c0e-bbf5-308aad6265af",
+#   country: Operator.Repo.get_by!(Operator.Common.Country, name: "FR"),
+#   jurisdiction: Operator.Repo.get_by!(Operator.Common.Jurisdiction, name: "MGA"),
+#   sub_partner_id: "123_sub",
+#   birth_date: "1980-10-20",
+#   registration_date: "2010-10-20",
+#   tags: [
+#     "VIP",
+#     "affiliate"
+#   ],
+#   sex: "MALE",
+#   affiliate_id: "124_af"
+# })
+
